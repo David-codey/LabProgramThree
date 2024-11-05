640c0fbb6fe24d78229cd27cc15c93dd0fce0745
David-codey <dfields42@tntech.edu>
2024-11-05 15:56:14 -0600

    Added in Overloaded Operator

--- Pokemon.h
+++ Pokemon.h
@@ -1,6 +1,8 @@
 #ifndef POKEMON_H
 #define POKEMON_H
 #include <string>
+#include <fstream>
+#include <iostream>
 using namespace std;
 class Pokemon
 {
@@ -28,7 +30,9 @@ public:
     void setID(int number){
         number = this->PokemonIndexNum;
     }
-    
+   friend ostream& operator<<(ostream& output, const Pokemon& pokemon){
+        output << "Pokemon #:" << pokemon.PokemonIndexNum << endl << "Pokemon Name:" << pokemon.PokemonName << endl;
+    }
 };
 
 #endif 
