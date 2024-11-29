fdcc1263dd1ecf568a8a6b40bb540ff066d42f47
David-codey <dfields42@tntech.edu>
2024-11-29 11:47:21 -0600

    Fixed error on line 38

--- BinaryTree.h
+++ BinaryTree.h
@@ -35,7 +35,7 @@ class BinaryTree{
         void displayInOrder(treeNode* nodePtr) const{
             if(nodePtr){
                 displayInOrder(nodePtr->left);
-                cout << *nodePtr->rootNode << endl;
+                cout << (*nodePtr)->rootNode << endl;
                 displayInOrder(nodePtr->right);
             }
         }
