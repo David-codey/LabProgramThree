9a542e736e00610112f43f6af3409cdef27307c9
David-codey <dfields42@tntech.edu>
2024-11-29 12:37:35 -0600

    Testing to see if it takes in the values from pokemon.txt

--- Program3.cpp
+++ Program3.cpp
@@ -18,6 +18,7 @@ int main(){
             getline(openPokedex,convertedPokeNum, '#');
             getline(openPokedex,pokeName, '#');
             Pokemon pokemon = Pokemon(pokeNum, pokeName);
+            cout << pokemon.getID(pokeNum) << pokemon.getName(pokeName) << endl;
             BT.insertNode(pokemon);
             if(pokemon.getID(pokeNum) == pokemon.getID(pokeNum)){
                 cout << "Oops!, This Pokemon with index" << pokemon.getID(pokeNum) << "is already in the Pokedex." << endl;
